245a76d03b00e8fa82c98bf1b7af7500c6e3c2d1
Oleksandr Lutsiuk <oleksandr.lutsiuk@gmail.com>
2022-04-24 03:23:52 +0300

    [TASK] add scroll function

--- design-module-css/src/components/FixButton/FixButton.js
+++ design-module-css/src/components/FixButton/FixButton.js
@@ -1,14 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import arrow from "../../img/arrow.svg";
 import style from "./FixButton.module.css";
 
 export const FixButton = () => {
+  const [visible, setVisible] = useState(false);
+  window.onscroll = function () {
+    scrollFunction();
+  };
+
+  function scrollFunction() {
+    if (
+      document.body.scrollTop > 20 ||
+      document.documentElement.scrollTop > 20
+    ) {
+      setVisible(true);
+    } else {
+      setVisible(false);
+    }
+  }
+
   function TopScroll() {
     document.documentElement.scrollTop = 0;
   }
   return (
     <div className={style.arrow}>
-      <img src={arrow} alt="" onClick={TopScroll}></img>
+      <img
+        src={arrow}
+        alt=""
+        onClick={TopScroll}
+        style={{ display: visible ? "block" : "none" }}
+      ></img>
     </div>
   );
 };
